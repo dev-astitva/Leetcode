6fccb33e4d1c381a9fa2ce2854d7a777a4d842e5
Astitva Srivastava <astitvsriv@gmail.com>
2026-08-04 11:25:01 +0530

    Time: 41 ms (12.05%), Space: 21.8 MB (94.92%) - LeetHub

--- Python3/Medium/2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
+++ Python3/Medium/2428-maximum-sum-of-an-hourglass/2428-maximum-sum-of-an-hourglass.py
@@ -3,12 +3,14 @@ class Solution:
         maxVal=0
         m,n=len(grid),len(grid[0])
         def calc(x,y):
-            total=0
-            for i in range(x,x+3):
-                for j in range(y,y+3):
-                    if i==x+1 and (j==y or j==y+2):
-                        continue
-                    total+=grid[i][j]
+            # total=0
+            # for i in range(x,x+3):
+            #     for j in range(y,y+3):
+            #         if i==x+1 and (j==y or j==y+2):
+            #             continue
+            #         total+=grid[i][j]
+            
+            total=grid[i][j]+grid[i][j+1]+grid[i][j+2]+grid[i+1][j+1]+grid[i+2][j]+grid[i+2][j+1]+grid[i+2][j+2]
             return total
 
         for i in range(m-2):
