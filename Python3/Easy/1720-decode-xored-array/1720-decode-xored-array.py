f5646393194ee510359485c88bea77d74f94e1b5
Astitva Srivastava <astitvsriv@gmail.com>
2026-09-07 19:17:31 +0530

    Time: 11 ms (10.2%), Space: 20.7 MB (72.66%) - LeetHub

--- Python3/Easy/1720-decode-xored-array/1720-decode-xored-array.py
+++ Python3/Easy/1720-decode-xored-array/1720-decode-xored-array.py
@@ -1,6 +1,6 @@
 class Solution:
     def decode(self, encoded: List[int], first: int) -> List[int]:
-        
+        '''
         #main target=encoded[0] from these arr[0]=first and arr[1] to find
         arrEl1=0
         while True:
@@ -9,5 +9,10 @@ class Solution:
             arrEl1+=1
         res=[first,arrEl1]
         for i in range(1,len(encoded)):
+            res+=[res[i]^encoded[i]]
+        return res
+        '''
+        res=[first]
+        for i in range(len(encoded)):
             res+=[res[i]^encoded[i]]
         return res
